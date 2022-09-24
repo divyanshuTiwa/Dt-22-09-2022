bc4104331a935ba01dad6504724e4b8756bce254
divyanshuTiwa <113118479+divyanshuTiwa@users.noreply.github.com>
2022-09-24 03:39:53 -0500

    Create Author.java

---
+++ Author.java
@@ -0,0 +1,26 @@
+package com.inheritanceinterfaces.bll;
+
+public class Author {
+ private String name;
+ private String email;
+ 
+ public Author(String name,String email) {
+	 this.name=name;
+	 this.email=email;
+}
+ public String getName() {
+	 return name;
+ }
+ public String getEmail() {
+	 return email;
+ }
+ public void setEmail(String email) {
+ this.email=email;
+ }
+ @Override
+ public String toString() {
+	 return "Author [Name= "+ name+",email " +email+"]";
+ }
+
+}
+
